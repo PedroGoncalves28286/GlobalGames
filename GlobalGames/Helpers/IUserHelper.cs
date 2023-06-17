a7fd79777f9212f8e4648b61c50a15808dcdb2d4
Pedro Gonçalves <PedroGoncalves28286@formandos.cinel.pt>
2023-06-17 17:56:17 +0100

    Change user profile and password

---
+++ GlobalGames/Helpers/IUserHelper.cs
@@ -0,0 +1,23 @@
+﻿using GlobalGames.Data.Entities;
+using GlobalGames.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace GlobalGames.Helpers
+{
+    public interface IUserHelper
+    {
+        Task<User> GetUserByEmailAsync(string email);
+        Task<IdentityResult> AddUserAsync(User user, string password);
+
+        Task<SignInResult> LoginAsync(LoginViewModel model);
+
+        Task LogoutAsync();
+
+        Task<IdentityResult> UpdateUserAsync(User user);
+
+        Task<IdentityResult> ChangePasswordAsync(User user, string oldPassword, string newPassword);
+
+
+    }
+}
---
+++ GlobalGames/Helpers/UserHelper.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading.Tasks;
+using GlobalGames.Data.Entities;
+using GlobalGames.Models;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace GlobalGames.Helpers
+{
+    public class UserHelper : IUserHelper
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public UserHelper(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public async Task<IdentityResult> AddUserAsync(User user, string password)
+        {
+            return await _userManager.CreateAsync(user, password);
+        }
+
+        public async Task<IdentityResult> ChangePasswordAsync(User user, string oldPassword, string newPassword)
+        {
+            return await _userManager.ChangePasswordAsync(user, oldPassword, newPassword);
+        }
+
+        public async Task<User> GetUserByEmailAsync(string email)
+        {
+            return await _userManager.FindByEmailAsync(email);
+        }
+
+        public async Task<SignInResult> LoginAsync(LoginViewModel model)
+        {
+            return await _signInManager.PasswordSignInAsync(
+               model.Username,
+               model.Password,
+               model.RememberMe,
+               false);
+        }
+
+        public async Task LogoutAsync()
+        {
+            await _signInManager.SignOutAsync();
+        }
+
+        public async Task<IdentityResult> UpdateUserAsync(User user)
+        {
+            return await _userManager.UpdateAsync(user);
+        }
+    }
+}
